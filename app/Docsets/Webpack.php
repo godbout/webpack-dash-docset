5d6b383afc41576aaf9da886684bdcfa8f92f798
Guill Lo <guill.bout@gmail.com>
2020-09-20 23:08:49 +0800

    add all api part as function and section entries. do not differentiate hook, method etc for now

--- app/Docsets/Webpack.php
+++ app/Docsets/Webpack.php
@@ -49,6 +49,7 @@ class Webpack extends BaseDocset
         $entries = collect();
 
         $entries = $entries->union($this->guideEntries($crawler, $file));
+        $entries = $entries->union($this->functionEntries($crawler, $file));
         $entries = $entries->union($this->optionEntries($crawler, $file));
         $entries = $entries->union($this->moduleEntries($crawler, $file));
         $entries = $entries->union($this->pluginEntries($crawler, $file));
@@ -138,6 +139,23 @@ class Webpack extends BaseDocset
         }
     }
 
+    protected function functionEntries(HtmlPageCrawler $crawler, string $file)
+    {
+        $entries = collect();
+
+        if (Str::contains($file, "{$this->url()}/api")) {
+            $crawler->filter('h2 > code, h3 > code')->each(function (HtmlPageCrawler $node) use ($entries, $file) {
+                $entries->push([
+                   'name' => $node->text(),
+                   'type' => 'Function',
+                   'path' =>  Str::after($file . '#' . Str::slug($node->text()), $this->innerDirectory())
+                ]);
+            });
+
+            return $entries;
+        }
+    }
+
     protected function moduleEntries(HtmlPageCrawler $crawler, string $file)
     {
         $entries = collect();
@@ -197,6 +215,22 @@ class Webpack extends BaseDocset
             $crawler->filter('h2 > code, h3 > code')->each(function (HtmlPageCrawler $node) {
                 $node->prepend(
                     '<a id="' . Str::slug($node->text()) . '" name="//apple_ref/cpp/Option/' . rawurlencode($node->text()) . '" class="dashAnchor"></a>'
+                );
+            });
+
+            $crawler->filter('h2 > a:first-child, h3 > a:first-child')->each(function (HtmlPageCrawler $node) {
+                $node->prepend(
+                    '<a id="' . Str::slug($node->parents()->first()->text()) . '" name="//apple_ref/cpp/Section/' . rawurlencode($node->parents()->first()->text()) . '" class="dashAnchor"></a>'
+                );
+            });
+
+            return;
+        }
+
+        if (Str::contains($file, $this->url() . '/api')) {
+            $crawler->filter('h2 > code, h3 > code')->each(function (HtmlPageCrawler $node) {
+                $node->prepend(
+                    '<a id="' . Str::slug($node->text()) . '" name="//apple_ref/cpp/Function/' . rawurlencode($node->text()) . '" class="dashAnchor"></a>'
                 );
             });
 
