57c49eff11fbb41f252905ae3041e97a9765a23d
Guill Lo <guill.bout@gmail.com>
2020-09-30 23:15:35 +0800

    fix issue with entries with underscores not getting highlighted
    this is a bug in Dash 5.4.1 itself. this commit is a hack around
    the issue.

--- app/Docsets/Webpack.php
+++ app/Docsets/Webpack.php
@@ -253,12 +253,9 @@ class Webpack extends BaseDocset
             ->before('<a name="//apple_ref/cpp/Section/Top" class="dashAnchor"></a>');
 
         $crawler->filter('h2, h3, h4')->each(function (HtmlPageCrawler $node) {
+            $node->prepend('<a id="' . Str::slug($node->text()) . '"></a>');
             $node->prepend(
-                '<a id="' . Str::slug(
-                    $node->text()
-                ) . '" name="//apple_ref/cpp/Section/' . rawurlencode(
-                    $node->text()
-                ) . '" class="dashAnchor"></a>'
+                '<a name="//apple_ref/cpp/Section/' . rawurlencode($node->text()) . '" class="dashAnchor"></a>'
             );
         });
     }
